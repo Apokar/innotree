6ccc2c84d8e39bff9e285c2d9b336215c66498f7
Administrator <apokar@163.com>
2018-02-11 16:25:55 +0800

    update

--- innotree_company_2.0.py
+++ innotree_company_2.0.py
@@ -409,21 +409,21 @@ if __name__ == '__main__':
     proxies = get_proxy()
     need_ids = get_id_fromDB()
 
-    # start_no = 0
-    # end_no = len(need_ids)
-    # thread_num = 5
-    # while start_no < (end_no - thread_num + 1):
-    #     threads = []
-    #
-    #     for inner_index in range(0, thread_num):
-    #         threads.append(threading.Thread(target=get_info, args=(need_ids[start_no + inner_index],)))
-    #     for t in threads:
-    #         t.setDaemon(True)
-    #         t.start()
-    #     t.join()
-    #     start_no += thread_num
-    # print '执行完毕  _@_ '+str(datetime.datetime.now())
-    id1 = '16636881613973489832'
-    id2 = '9172814686291618768'
-    id3 = '11636707478071503398'  # 没有产品信息的
-    get_info(id1)
+    start_no = 0
+    end_no = len(need_ids)
+    thread_num = 5
+    while start_no < (end_no - thread_num + 1):
+        threads = []
+
+        for inner_index in range(0, thread_num):
+            threads.append(threading.Thread(target=get_info, args=(need_ids[start_no + inner_index],)))
+        for t in threads:
+            t.setDaemon(True)
+            t.start()
+        t.join()
+        start_no += thread_num
+    print '执行完毕  _@_ '+str(datetime.datetime.now())
+    # id1 = '16636881613973489832'
+    # id2 = '9172814686291618768'
+    # id3 = '11636707478071503398'  # 没有产品信息的
+    # get_info(id1)
